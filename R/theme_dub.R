4bd3b023ca2af50c8d73fb2b6433dd81908b1a0d
Robert Bilyk <bilykr@mail.gvsu.edu>
2022-06-15 17:12:10 +0000

    initial du bois theme

---
+++ R/theme_dub.R
@@ -0,0 +1,57 @@
+#' Du Bois Theme
+#'
+#' @return A graph resembles the 1890 Statistical Atlas from Henry Gannett.
+#' @export
+#'
+#' @examples library("ggplot2")
+#'
+#' ggplot(data = Titanic, aes(x = Class, y = Freq, fill = Survived)) +
+#' geom_col() +
+#' theme_hga()
+theme_hga <- function(){
+  sysfonts::font_add_google(name = "Puritan", family = "Main")
+  showtext::showtext_auto()
+
+  font <- "Main"
+
+  theme_minimal() %+replace%
+
+    theme(
+
+      #grid elements
+      panel.grid.major = element_line(color = "black", size = .25),
+      panel.grid.minor = element_blank(),
+
+      panel.background = element_rect(color="black", fill= "#dec8b1"),
+      plot.background = element_rect(fill = "#dec8b1"),
+
+      plot.title = element_text(
+        family = font,
+        size = 7,
+        face = 'bold'),
+
+      plot.subtitle = element_text(
+        family = font,
+        size = 7),
+
+      plot.tag = element_text(
+        family = font,
+        size = 7),
+
+      plot.caption = element_text(
+        family = font,
+        size = 9),
+
+      axis.title = element_blank(),
+
+      axis.text.y = element_text(
+        family = font,
+        size = 8,
+        color = "black"),
+
+      axis.text.x = element_text(
+        family = font,
+        size = 7,
+        color = "black")
+    )
+}
