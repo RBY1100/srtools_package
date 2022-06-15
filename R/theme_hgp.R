ec2d59d40cfdeb6f3eba2359f9ee45b8e716bc92
Robert Bilyk <bilykr@mail.gvsu.edu>
2022-06-15 16:42:58 +0000

    initial work

---
+++ R/theme_hgp.R
@@ -0,0 +1,57 @@
+sysfonts::font_add_google(name = "Courier Prime", family = "Main")
+showtext::showtext_auto()
+
+#' Henry Gannett Atlas Theme
+#'
+#' @return A graph resembles the 1890 Statistical Atlas from Henry Gannett.
+#' @export
+#'
+#' @examples library("ggplot2")
+#'
+#' ggplot(data = Titanic, aes(x = Class, y = Freq, fill = Survived)) +
+#' geom_col() +
+#' theme_hga()
+theme_hga <- function(){
+  font <- "Main"
+
+  theme_minimal() %+replace%
+
+    theme(
+
+      #grid elements
+      panel.grid.major = element_line(color = "black", size = .25),
+      panel.grid.minor = element_blank(),
+
+      panel.background = element_rect(color="black", fill= "#f1d9b5"),
+      plot.background = element_rect(fill = "#f1d9b5"),
+
+      plot.title = element_text(
+        family = font,
+        size = 7,
+        face = 'bold'),
+
+      plot.subtitle = element_text(
+        family = font,
+        size = 7),
+
+      plot.tag = element_text(
+        family = font,
+        size = 7),
+
+      plot.caption = element_text(
+        family = font,
+        size = 9),
+
+      axis.title = element_blank(),
+
+      axis.text.y = element_text(
+        family = font,
+        size = 8,
+        color = "black"),
+
+      axis.text.x = element_text(
+        family = font,
+        size = 7,
+        color = "black")
+    )
+}
